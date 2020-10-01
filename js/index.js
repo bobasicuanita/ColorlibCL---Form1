47db280bf2c93bea8cbe542eb2f187692f67bcd3
konstantinos karasavvas <konstantinoskarasavvas@hotmail.com>
2020-10-01 20:03:59 +0300

    Added listeners

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+/******* VARIABLE DECLARATIONS ******************/
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const subject = document.getElementById('subject');
@@ -6,13 +8,23 @@ const messages = document.querySelectorAll('.message');
 const onSubmit = document.querySelector('.btn');
 const validateInput = document.querySelectorAll('.validate-input');
 const error = document.querySelectorAll('.error');
+const input = document.querySelectorAll('input');
+const textarea = document.querySelector('textarea');
 
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+
+/******* FUNCTION DECLARATIONS ******************/
 const getMessage = (index, message) => {
     return messages[index].innerHTML = `${message} <i class="fas fa-exclamation-circle"></i>`;
 }
 
+const erase = (index) => {
+    error[index].style.display = 'none';
+    messages[index].textContent = ''; 
+}
+
+/******* EVENT LISTENERS ******************/
 onSubmit.addEventListener('click', () => {
 
     for (i = 0; i < messages.length; i++) {
@@ -37,3 +49,8 @@ onSubmit.addEventListener('click', () => {
         getMessage(3, 'You must enter your message.');
     }
 });
+
+input[0].addEventListener('focus', () => erase(0));
+input[1].addEventListener('focus', () => erase(1));
+input[2].addEventListener('focus', () => erase(2));
+textarea.addEventListener('focus', () => erase(3));
